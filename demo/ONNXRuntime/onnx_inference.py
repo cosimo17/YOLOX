8ba0c8c81f3f5e175a9da72302e90eee259d9d99
cosimo17 <526174835@qq.com>
2022-04-19 16:46:33 +0800

    improve onnx inference script

--- demo/ONNXRuntime/onnx_inference.py
+++ demo/ONNXRuntime/onnx_inference.py
@@ -63,30 +63,41 @@ if __name__ == '__main__':
     args = make_parser().parse_args()
 
     input_shape = tuple(map(int, args.input_shape.split(',')))
-    origin_img = cv2.imread(args.image_path)
-    img, ratio = preprocess(origin_img, input_shape)
+    imgnames = []
+    # Test with one image or with images in some dir
+    if os.path.isdir(args.image_path):
+        imgnames = os.listdir(args.image_path)
+        imgnames = [os.path.join(args.image_path, f) for f in imgnames]
+    else:
+        imgnames = [args.image_path]
+    TP = 0
+    for count, imgname in enumerate(imgnames):
+        print("Inference with {}th/{} images".format(count, len(imgnames)))
+        origin_img = cv2.imread(imgname)
+        img, ratio = preprocess(origin_img, input_shape)
 
-    session = onnxruntime.InferenceSession(args.model)
+        session = onnxruntime.InferenceSession(args.model)
 
-    ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
-    output = session.run(None, ort_inputs)
-    predictions = demo_postprocess(output[0], input_shape, p6=args.with_p6)[0]
+        ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
+        output = session.run(None, ort_inputs)
+        predictions = demo_postprocess(output[0], input_shape, p6=args.with_p6)[0]
 
-    boxes = predictions[:, :4]
-    scores = predictions[:, 4:5] * predictions[:, 5:]
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
 
-    boxes_xyxy = np.ones_like(boxes)
-    boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
-    boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
-    boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
-    boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
-    boxes_xyxy /= ratio
-    dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
-    if dets is not None:
-        final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
-        origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
-                         conf=args.score_thr, class_names=COCO_CLASSES)
-
-    mkdir(args.output_dir)
-    output_path = os.path.join(args.output_dir, os.path.basename(args.image_path))
-    cv2.imwrite(output_path, origin_img)
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= ratio
+        dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
+        if dets is not None:
+            final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
+            origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
+                             conf=args.score_thr, class_names=COCO_CLASSES)
+            TP += 1
+        mkdir(args.output_dir)
+        output_path = os.path.join(args.output_dir, os.path.basename(imgname))
+        cv2.imwrite(output_path, origin_img)
+    print("TP:{}  Number of test images:{}".format(TP, len(imgnames)))
