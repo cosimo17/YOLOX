e76570a9398576f0cf3a78d1272c6705691f443c
cosimo17 <526174835@qq.com>
2022-04-19 16:46:15 +0800

    support Post Training Quantization

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -4,6 +4,8 @@
 
 import argparse
 import os
+
+import cv2
 from loguru import logger
 
 import torch
@@ -12,6 +14,60 @@ from torch import nn
 from yolox.exp import get_exp
 from yolox.models.network_blocks import SiLU
 from yolox.utils import replace_module
+from pytorch_quantization import nn as quant_nn
+from pytorch_quantization import calib
+from pytorch_quantization.tensor_quant import QuantDescriptor
+from pytorch_quantization import quant_modules
+from yolox.data.data_augment import preproc as preprocess
+import os
+import random
+
+
+def collect_stats(model, sample_dir, sample_numbers):
+    # list all imgs
+    imgnames = os.listdir(sample_dir)
+    imgnames = [os.path.join(sample_dir, imgname) for imgname in imgnames]
+    random.shuffle(imgnames)
+
+    print("start to collect quantization parameters")
+    for name, module in model.named_modules():
+        if isinstance(module, quant_nn.TensorQuantizer):
+            if module._calibrator is not None:
+                module.disable_quant()
+                module.enable_calib()
+            else:
+                module.disable()
+    for i in range(sample_numbers):
+        print("Collect quantization parametes with {}th/{} sample".format(i+1, sample_numbers))
+        imgname = imgnames[i]
+        img = cv2.imread(imgname)
+        img, _ = preprocess(img, (640, 640))
+        img = torch.from_numpy(img).unsqueeze(0).float().cuda()
+        with torch.cuda.amp.autocast(False):
+            model(img)
+
+    # Disable calibrators
+    for name, module in model.named_modules():
+        if isinstance(module, quant_nn.TensorQuantizer):
+            if module._calibrator is not None:
+                module.enable_quant()
+                module.disable_calib()
+            else:
+                module.enable()
+
+
+def compute_amax(model, **kwargs):
+    print("start to compute quantization parameters")
+    # Load calib result
+    for name, module in model.named_modules():
+        if isinstance(module, quant_nn.TensorQuantizer):
+            if module._calibrator is not None:
+                if isinstance(module._calibrator, calib.MaxCalibrator):
+                    module.load_calib_amax()
+                else:
+                    module.load_calib_amax(**kwargs)
+            print(F"{name:40}: {module}")
+    model.cuda()
 
 
 def make_parser():
@@ -54,7 +110,12 @@ def make_parser():
         action="store_true",
         help="decode in inference or not"
     )
-
+    parser.add_argument(
+        '--quantization',
+        action='store_true',
+        help='Do post training quantization before exporting.'
+    )
+    parser.add_argument('--represent_dataset', '-rd', type=str, help='Path of the represent dataset used for Post Training Quantization.')
     return parser
 
 
@@ -68,6 +129,12 @@ def main():
     if not args.experiment_name:
         args.experiment_name = exp.exp_name
 
+    if args.quantization: # enable post training quantization
+        quant_nn.TensorQuantizer.use_fb_fake_quant = True
+        quant_modules.initialize()
+        quant_desc_input = QuantDescriptor(calib_method='histogram')
+        quant_nn.QuantConv2d.set_default_quant_desc_input(quant_desc_input)
+        quant_nn.QuantLinear.set_default_quant_desc_input(quant_desc_input)
     model = exp.get_model()
     if args.ckpt is None:
         file_name = os.path.join(exp.output_dir, args.experiment_name)
@@ -77,17 +144,23 @@ def main():
 
     # load the model state dict
     ckpt = torch.load(ckpt_file, map_location="cpu")
-
     model.eval()
     if "model" in ckpt:
         ckpt = ckpt["model"]
     model.load_state_dict(ckpt)
     model = replace_module(model, nn.SiLU, SiLU)
     model.head.decode_in_inference = args.decode_in_inference
+    model.cuda()
+
+    # Post Training Quantization
+    if args.quantization:
+        with torch.no_grad():
+            collect_stats(model, args.represent_dataset, 10000)
+            compute_amax(model, method="percentile", percentile=99.99)
 
     logger.info("loading checkpoint done.")
-    dummy_input = torch.randn(args.batch_size, 3, exp.test_size[0], exp.test_size[1])
-
+    dummy_input = torch.randn(args.batch_size, 3, exp.test_size[0], exp.test_size[1]).cuda()
+    model.train()
     torch.onnx._export(
         model,
         dummy_input,
@@ -96,7 +169,9 @@ def main():
         output_names=[args.output],
         dynamic_axes={args.input: {0: 'batch'},
                       args.output: {0: 'batch'}} if args.dynamic else None,
-        opset_version=args.opset,
+        opset_version=13,
+        verbose=False,
+        training=torch.onnx.TrainingMode.EVAL
     )
     logger.info("generated onnx model named {}".format(args.output_name))
 
